30cb7290592b28382abbafe6d3b6cd5abfcadb74
Gladiator-0007 <hitakshakhaja08@gmail.com>
2026-06-21 16:22:03 +0530

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -8,7 +8,4 @@ cloudinary.config({
     api_secret:process.env.API_SECRET
 });
 
-console.log("CLOUD_NAME =", process.env.CLOUD_NAME);
-console.log("API_KEY =", process.env.API_KEY);
-console.log("API_SECRET =", process.env.API_SECRET);
 export default cloudinary;
